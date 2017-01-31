d0c3387aa80ba87654f3cf5a814343aeb64f1ebb
deglot <dbetancourt@easysol.net>
2017-01-31 11:31:01 -0500

    Update docker-entrypoint.sh
    Adding java -jar here so that env var set in a docker-compose can be read/access in here.
    
    Sleep of 10 to allow extra time before entering If to create console user.

--- server/docker-entrypoint.sh
+++ server/docker-entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+java -jar /usr/share/java/saxon.jar -s:/opt/jboss/keycloak/standalone/configuration/standalone.xml -xsl:/opt/jboss/keycloak/setLogLevel.xsl -o:/opt/jboss/keycloak/standalone/configuration/standalone.xml
+
+sleep 10
+
 if [ $KEYCLOAK_USER ] && [ $KEYCLOAK_PASSWORD ]; then
     keycloak/bin/add-user-keycloak.sh --user $KEYCLOAK_USER --password $KEYCLOAK_PASSWORD
 fi
